7f1a95546e7d2040925098a8c1ba28150950163c
D3stroyer-xD <96006825+D3stroyer-xD@users.noreply.github.com>
2021-12-20 15:58:34 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -23,7 +23,7 @@ from yt_dlp.utils import (
 
 @Client.on_message(filters.command("song") & ~filters.edited)
 async def song(client, message):
-    cap = "**ꜱᴏɴɢ ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ ꜱᴜᴍɪᴛ ʏᴀᴅᴀᴠ...**"
+    cap = "**ꜱᴏɴɢ ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ @AltronixMusicBot..**"
     url = message.text.split(None, 1)[1]
     rkp = await message.reply("**🎧...**")
     if not url:
